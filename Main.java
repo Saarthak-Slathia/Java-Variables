d5a56d89a17df4be875362453e66df82611ddf95
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-01-25 10:12:08 +0530

    Add files via upload
    Variables of Java.

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    Variables in Java:-
+        /*
+        A variable is like a container to store values.
+        Example:-
+            We keep candies in a box, in this case, candies are a value and box is a container.
+        Similarly in Java we have variables to store variables.
+
+        We define variable with the syntax:-
+        var name_of_variable = value;
+
+        Example:-
+         */
+        int a = 21; // Here a is an int or integer type variable with value 21.
+        String b = "Hello World"; // Here b is a string type variable with value "Hello World".
+        /*
+        Variable rules:-
+            1. Variable's name cannot begin with special characters and numbers except for '_'.
+            2. There cannot be 2 variables with the same name.
+            3. Variable name should not be a java keyword, eg - class, byte etc.
+            4. The variable type should be suitable, or if you are not sure of the variable type, use the keyword var.
+            Example:-
+         */
+        var Variable = "Use var if you are not sure.";
+        var num = 21;
+
+        // Thank You !!!
+    }
+}
